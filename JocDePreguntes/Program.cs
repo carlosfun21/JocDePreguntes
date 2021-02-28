67d70271fda461835d937e30ab8d6ed23cdf34e2
carlos <carlosaarman@gmail.com>
2021-02-28 22:46:56 +0100

    Versió Pokemon i Operacions

--- JocDePreguntes/Program.cs
+++ JocDePreguntes/Program.cs
@@ -9,27 +9,33 @@ namespace JocDePreguntes
             int eleccio;
 
             mostrarMenu();
-            Console.WriteLine("Tria el tema : ");
+            Console.WriteLine("Per triar el tema s'ha de escriure el numero assignat a cada tema.\nTria el tema : ");
             eleccio = Convert.ToInt32(Console.ReadLine());
 
             while (eleccio != 0)
             {
                 if (eleccio == 1) menuPokemon();
-                
-                Console.WriteLine("Pots triar un altre tema. En cas de voler sortir polsa 0");
+                if (eleccio == 2) menuOperacions();
+                //if (eleccio == 3) menuSmite();
+
+
+                Console.WriteLine("Pots triar un altre tema. En cas de voler sortir polsa 0 \n");
+                mostrarMenu();
                 eleccio = Convert.ToInt32(Console.ReadLine());
+
 
             }
 
         }
-        static void mostrarMenu()
+        public static void mostrarMenu()
         {
             Console.WriteLine("Tema 1: Pokemon");
             Console.WriteLine("Tema 2: Operacions");
             Console.WriteLine("Tema 3: Smite");
 
         }
-        static void menuPokemon()
+        
+        public static void menuPokemon()
         {
             char resposta;
             int comptador = 0;
@@ -72,9 +78,50 @@ namespace JocDePreguntes
 
             Console.WriteLine("El teu resultat és de {0} / 5 ",comptador);
 
+        }
 
+        public static void menuOperacions()
+        {
+            char resposta;
+            int comptador = 0;
+            Console.WriteLine("Tema d'Operacions! Endivina quina es la resposta correcta de les 5 preguntes seguents. \n");
 
+            Console.WriteLine("Pregunta 1: Quant és 1+1 ?");
+            Console.WriteLine(" A) 2 \n B) 1 \n C) 0 \n D) 11 \n");
+            Console.Write("La teva resposta és: ");
+            resposta = Convert.ToChar(Console.ReadLine());
+            if (resposta == 'A' || resposta == 'a') { Console.WriteLine("Correcte! \n"); comptador++; }
+            else Console.WriteLine("Incorrecte :( \n");
 
+            Console.WriteLine("Pregunta 2: Quant és 453634-1 ?");
+            Console.WriteLine(" A) 435643 \n B) 453633 \n C) 453654 \n D) 345635 \n");
+            Console.Write("La teva resposta és: ");
+            resposta = Convert.ToChar(Console.ReadLine());
+            if (resposta == 'B' || resposta == 'b') { Console.WriteLine("Correcte! \n"); comptador++; }
+            else Console.WriteLine("Incorrecte :( \n");
+
+            Console.WriteLine("Pregunta 3: Quant és 34 x 2 ?");
+            Console.WriteLine(" A) 68 \n B) 0 \n C) 16 \n D) 342 \n");
+            Console.Write("La teva resposta és: ");
+            resposta = Convert.ToChar(Console.ReadLine());
+            if (resposta == 'A' || resposta == 'a') { Console.WriteLine("Correcte! \n"); comptador++; }
+            else Console.WriteLine("Incorrecte :( \n");
+
+            Console.WriteLine("Pregunta 4: Quant és 100 / 1");
+            Console.WriteLine(" A) 50 \n B) 10 \n C) 1 \n D) 100 \n");
+            Console.Write("La teva resposta és: ");
+            resposta = Convert.ToChar(Console.ReadLine());
+            if (resposta == 'D' || resposta == 'd') { Console.WriteLine("Correcte! \n"); comptador++; }
+            else Console.WriteLine("Incorrecte :( \n");
+
+            Console.WriteLine("Pregunta 5: Quant és 3 + (3 x 3) ?");
+            Console.WriteLine(" A) 18 \n B) 6 \n C) 12 \n D) 3 \n");
+            Console.Write("La teva resposta és: ");
+            resposta = Convert.ToChar(Console.ReadLine());
+            if (resposta == 'C' || resposta == 'c') { Console.WriteLine("Correcte! \n"); comptador++; }
+            else Console.WriteLine("Incorrecte :( \n");
+
+            Console.WriteLine("El teu resultat és de {0} / 5 ", comptador);
         }
     }
 }
